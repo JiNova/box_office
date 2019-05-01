05d987e2a0459162c39c2b19216d53c9771fa197
Andreas Brandner <abrandner@uchicago.edu>
2019-05-01 02:58:40 -0500

    green

--- db_test.go
+++ db_test.go
@@ -39,16 +39,16 @@ func TestData_FillModels(t *testing.T) {
 	}
 }
 
-func TestData_LoadAssociations(t *testing.T) {
+func TestData_LoadAssociationsSingle(t *testing.T) {
 	var data Data
 	data.Init()
 	defer data.Close()
 
 	var mov Movie
-	mov.Shows = []Show{}
 	expectedShowNum := 5
 	movieId := 5
 	data.FillModelById(&mov, movieId)
+	mov.Shows = []Show{}
 	err := data.LoadAssociations(&mov, "Shows")
 
 	if err != nil {
@@ -61,6 +61,36 @@ func TestData_LoadAssociations(t *testing.T) {
 
 	for i, show := range mov.Shows {
 		if show.MovieID != 5 {
+			t.Error("Show", i, "belongs to another movie!")
+		}
+	}
+}
+
+func TestData_LoadAssociationsMulti(t *testing.T) {
+	var data Data
+	data.Init()
+	defer data.Close()
+
+	var movs []Movie
+	expectedShowNum := 5
+	verifyMovIndex := 4
+	data.FillModels(&movs)
+	for _ , mov := range movs {
+		mov.Shows = []Show{}
+	}
+
+	err := data.LoadAssociations(&movs, "Shows")
+
+	if err != nil {
+		t.Error("Could not load shows", err)
+	} else if movs[verifyMovIndex].Shows == nil {
+		t.Error("Show data not loaded successfully")
+	} else if len(movs[verifyMovIndex].Shows) != expectedShowNum {
+		t.Error("Expected ", expectedShowNum, "got", len(movs[verifyMovIndex].Shows))
+	}
+
+	for i, show := range movs[verifyMovIndex].Shows {
+		if show.MovieID != uint(verifyMovIndex+1) {
 			t.Error("Show", i, "belongs to another movie!")
 		}
 	}
