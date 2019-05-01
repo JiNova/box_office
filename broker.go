3dd8b86095a61a30e2edf1b768604bf13696459b
Andreas Brandner <abrandner@uchicago.edu>
2019-05-01 08:24:43 -0500

    refactor

--- broker.go
+++ broker.go
@@ -1,5 +1,7 @@
 package main
 
+import "time"
+
 type DataBroker struct {
 	dbhandler DBHandler
 }
@@ -10,4 +12,27 @@ func (broker *DataBroker) Init() {
 
 func (broker *DataBroker) Close() {
 	broker.dbhandler.Close()
+}
+
+func (broker *DataBroker) GetAvailableTickets(date *time.Time, show *Show) (avail []int) {
+	var all int
+	var tickets []Ticket
+
+	all, err := broker.dbhandler.CountEntries(&Tier{})
+	if err != nil {
+		panic(err)
+	}
+
+	broker.dbhandler.db.Where("date = ? AND show_id = ?", date, show.ShowID).Find(&tickets)
+	avail = make([]int, all)
+
+	for i:= range avail {
+		avail[i] = 10
+	}
+
+	for _, ticket := range tickets {
+		avail[ticket.TierID-1]--
+	}
+
+	return
 }
