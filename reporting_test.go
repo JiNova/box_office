c9dcc7db9623ffc323f2071d9aeea06d81d0d31c
Andreas Brandner <abrandner@uchicago.edu>
2019-05-07 15:45:28 -0500

    green

--- reporting_test.go
+++ reporting_test.go
@@ -23,3 +23,21 @@ func TestReportHandler_GetDateFromUser(t *testing.T) {
 		t.Error("Wrong date returned, expected", expectedDate, "got", date)
 	}
 }
+
+func TestReportHandler_GetTimeFromUser(t *testing.T) {
+	var broker DataBroker
+	broker.Init()
+
+	reporter := ReportHandler{&broker}
+	oldStdin := os.Stdin
+	inputfile := emulateUserInput("7 pm")
+	expectedTime, _ := time.Parse("3 pm", "7 pm")
+
+	defer os.Remove(inputfile.Name())      // clean up
+	defer func() { os.Stdin = oldStdin }() // Restore stdin
+	defer broker.Close()
+
+	if time := reporter.GetTimeFromUser(); !time.Equal(expectedTime) {
+		t.Error("Wrong date returned, expected", expectedTime, "got", time)
+	}
+}
