87209d56b8ea85520fb1e1f0bcfc4e5605356e28
Andreas Brandner <abrandner@uchicago.edu>
2019-05-07 14:40:07 -0500

    green

--- refunding_test.go
+++ refunding_test.go
@@ -1,8 +1,25 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
 func TestRefundHandler_GetSerialsFromUser(t *testing.T) {
+	var broker DataBroker
+	broker.Init()
+	refunder := RefundHandler{&broker}
+
+	oldStdin := os.Stdin
+	inputfile := emulateUserInput("aaabbb sdafdas asdfsdff adsffssdf")
+
+	defer os.Remove(inputfile.Name())      // clean up
+	defer func() { os.Stdin = oldStdin }() // Restore stdin
+	defer broker.Close()
+
+	if serials, err := refunder.GetSerialsFromUser(); err != nil {
+		t.Error("Error when trying to get serials, got", err)
+	} else if len(serials) != 4 {
+		t.Error("Wrong number of serials, expected 4, got", len(serials))
+	}
 }
